94676e4ebe57a49dece539dfdaa7e4270c953438
Verevkina T <verevkina@picom.ru>
2018-03-26 19:14:58 +0400

    header-fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
       $("#my-menu").mmenu();
       $("#mm-1").append('<div class="header-contacts"><div class="header-tel"><a href="tel:+73414547115"><svg role="img" width="30" height="30" class="phone"><use xlink:href="#telephone"></use></svg><span class="num">+7 (34145) <span class="weight">47-115</span></span></a></div><div class="header-text"><p><svg role="img" width="15" height="15"><use xlink:href="#local"></use></svg><span>Адрес: </span><a href="mailto:sigmau@mail.ru">г. Воткинск, ул. Мира, 1а</a></p><p><svg role="img" width="15" height="15"><use xlink:href="#email"> </use></svg><span>E-mail: </span><a href="mailto:sigmau@mail.ru">sigmau@mail.ru</a></p></div></div>');
 });
+var productcat
+$(".product-name").on("click", function(){
+  productcat = this.closest(".productcat-item");
+  if($(productcat).hasClass("productcat-active")){
+    $(productcat).removeClass("productcat-active");
+  }
+  else{
+    $(".productcat-item").removeClass("productcat-active");
+    productcat.classList.add("productcat-active");
+  }
+});
 $("#menuOpen").on("click", function(){
   if($("#menuOpen").hasClass("open")){
     $("#my-menu").hide();
@@ -14,20 +25,35 @@ $("#menuOpen").on("click", function(){
     $("#menuOpen").addClass("open");
   }
 });
+
+
 $(window).on("load",function(){
   $(".scroll-container").mCustomScrollbar({
     axis:"x",
-    theme: "3d-thick-dark"
+    theme: "my-theme",
+    scrollButtons:{ enable: true }
     });
-  });
 $('.slider').slick({
   prevArrow:'<svg role="img" width="40" height="20" class="arrowleft"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
   nextArrow:'<svg role="img" width="40" height="20" class="arrowright"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
-  appendArrows:$('.slider-arrows')
+  appendArrows:$('.slider-arrows'),
+  infinite:false
 });
 $('.productslider').slick({
   dots: true,
-  arrows: false
+  arrows: false,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      dots: false,
+      arrows: true,
+      prevArrow:'<svg role="img" width="40" height="20" class="arrowleft"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
+      nextArrow:'<svg role="img" width="40" height="20" class="arrowright"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
+      appendArrows:$('.slider-arrows'),
+      infinite:false
+    }
+ ]
 });
 $('.certificate-slider').slick({
   dots: true,
@@ -48,7 +74,7 @@ $('.certificate-slider').slick({
     }
   ]
 });
-$('img.open-img').magnificPopup({
+$('open-img').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		closeOnContentClick: false,
@@ -74,6 +100,7 @@ $('img.open-img').magnificPopup({
 $('.openform').magnificPopup({
   type: 'ajax'
 });
+});
 var tab = document.getElementsByClassName('tabs-btn');
 var tabContent = document.getElementsByClassName('tabs-inner');
 for (var i=0 ; i<tab.length; i++) {
@@ -91,3 +118,12 @@ for (var i=0 ; i<tab.length; i++) {
   currentTab.add('tabs-inner__current');
   });
 }
+$(".share-btn").on("click", function(){
+  var conteiner = $('.share-btn').closest(".share-container");
+  conteiner.toggleClass('share-open')
+});
+
+ $('.bounceInLeft').addClass("animated");
+ $('.up').on("click", function(){
+   $('html, body').animate({scrollTop:0}, 'slow');
+ })
