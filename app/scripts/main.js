177a02931232bc2a4c4115c35bd52dde17fd1718
Verevkina T <verevkina@picom.ru>
2018-03-27 18:31:58 +0400

    aos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
       $("#my-menu").mmenu();
       $("#mm-1").append('<div class="header-contacts"><div class="header-tel"><a href="tel:+73414547115"><svg role="img" width="30" height="30" class="phone"><use xlink:href="#telephone"></use></svg><span class="num">+7 (34145) <span class="weight">47-115</span></span></a></div><div class="header-text"><p><svg role="img" width="15" height="15"><use xlink:href="#local"></use></svg><span>Адрес: </span><a href="mailto:sigmau@mail.ru">г. Воткинск, ул. Мира, 1а</a></p><p><svg role="img" width="15" height="15"><use xlink:href="#email"> </use></svg><span>E-mail: </span><a href="mailto:sigmau@mail.ru">sigmau@mail.ru</a></p></div></div>');
-});
 var productcat
 $(".product-name").on("click", function(){
   productcat = this.closest(".productcat-item");
@@ -26,8 +25,6 @@ $("#menuOpen").on("click", function(){
   }
 });
 
-
-$(window).on("load",function(){
   $(".scroll-container").mCustomScrollbar({
     axis:"x",
     theme: "my-theme",
@@ -38,22 +35,6 @@ $('.slider').slick({
   nextArrow:'<svg role="img" width="40" height="20" class="arrowright"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
   appendArrows:$('.slider-arrows'),
   infinite:false
-});
-$('.productslider').slick({
-  dots: true,
-  arrows: false,
-  mobileFirst: true,
-  responsive: [
-    {
-      breakpoint: 1200,
-      dots: false,
-      arrows: true,
-      prevArrow:'<svg role="img" width="40" height="20" class="arrowleft"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
-      nextArrow:'<svg role="img" width="40" height="20" class="arrowright"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
-      appendArrows:$('.slider-arrows'),
-      infinite:false
-    }
- ]
 });
 $('.certificate-slider').slick({
   dots: true,
@@ -74,17 +55,29 @@ $('.certificate-slider').slick({
     }
   ]
 });
-$('open-img').magnificPopup({
-		delegate: 'a',
+$('.productslider').slick({
+  dots: true,
+  arrows: false,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      dots: false,
+      arrows: true,
+      prevArrow:'<svg role="img" width="40" height="20" class="arrowleft"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
+      nextArrow:'<svg role="img" width="40" height="20" class="arrowright"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow"> </use></svg>',
+      appendArrows:$('.slider-arrows'),
+      infinite:false
+    }
+ ]
+});
+$('.open-img').magnificPopup({
 		type: 'image',
 		closeOnContentClick: false,
 		closeBtnInside: false,
 		mainClass: 'mfp-with-zoom mfp-img-mobile',
 		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
+			verticalFit: true
 		},
 		gallery: {
 			enabled: true
@@ -99,7 +92,6 @@ $('open-img').magnificPopup({
 	});
 $('.openform').magnificPopup({
   type: 'ajax'
-});
 });
 var tab = document.getElementsByClassName('tabs-btn');
 var tabContent = document.getElementsByClassName('tabs-inner');
@@ -122,7 +114,10 @@ $(".share-btn").on("click", function(){
   var conteiner = $('.share-btn').closest(".share-container");
   conteiner.toggleClass('share-open')
 });
-AOS.init()
+AOS.init({
+  disable: window.innerWidth < 1024
+});
  $('.up').on("click", function(){
    $('html, body').animate({scrollTop:0}, 'slow');
- })
+ });
+});
